1a448a762c72b68f28b22d6daf30671722b86bf9
Oleksiy Kholiavko <o.kholjavko@yandex.ua>
2018-12-06 12:13:15 +0200

    remove 'full' from args

--- scraper.py
+++ scraper.py
@@ -5,19 +5,18 @@
 Odds portal scraper
 
 Usage:
-    scraper.py full <mhsh> <seas> [<fpage> <lpage>]
-    scraper.py last <mhsh> <days>
-    scraper.py each
+    scraper.py <mhsh> <seas> [<fpage> <lpage>]
+    scraper.py <mhsh> <days>
     scraper.py -d | --debug
     scraper.py -v | --version
     scraper.py -h | --help
 
 Options:
-    python scraper.py full bbusnb 2018-2019
-    python scraper.py full bsusml 2016
-    -d --debug          Show debug messages.
-    -h --help           Show this screen.
-    -v --version        Show version.
+    python scraper.py bbusnb 2018-2019
+    python scraper.py bsusml 2016
+    -d --debug        Show debug messages.
+    -h --help         Show this screen.
+    -v --version      Show version.
 """
 
 from docopt import docopt
@@ -44,52 +43,43 @@ season = args['<seas>']
 mhsh = args['<mhsh>']
 module = mhsh_dict[mhsh]
 
-if args['full'] is True:
-    # if first and last page was not defined
-    # create it like 1 and 50
-    first = 1 if args['<fpage>'] is None else int(args['<fpage>'])
-    last = 50 if args['<lpage>'] is None else int(args['<lpage>'])
 
-    #  check if page numb arguments is correct
-    if last < first:
-        raise Exception('First page is bigger than last!')
+# if first and last page was not defined
+# create it like 1 and 50
+first = 1 if args['<fpage>'] is None else int(args['<fpage>'])
+last = 50 if args['<lpage>'] is None else int(args['<lpage>'])
 
-    """   SCRAP RESULTS PAGE   """
-    diapazon = range(first, last)
-    text = 'Start season {} from {} to {} page'
-    log_main.info(text.format(season, first, last))
+#  check if page numb arguments is correct
+if last < first:
+    raise Exception('First page is bigger than last!')
 
-    """  DO SMTH  """
-    tags_list = get_table(module, season, diapazon)
+"""   SCRAP RESULTS PAGE   """
+diapazon = range(first, last)
+text = 'Start season {} from {} to {} page'
+log_main.info(text.format(season, first, last))
 
-    # if tags_list is not None:
-    if tags_list:
-        print('\n\n\n if tag list \n\n\n')
+"""  DO SMTH  """
+tags_list = get_table(module, season, diapazon)
+
+# if tags_list is not None:
+if tags_list:
+    print('\n\n\n if tag list \n\n\n')
+    ###################
+    #   row by rows   #
+    ###################
+    for y, tag_arr in enumerate(tags_list):
+        seas_type, xeid, bs4_tag = tag_arr
+        print('\n\n\n for y, tag_arr in enumerate(tags_list): \n\n\n')
         ###################
-        #   row by rows   #
+        #   check xeid    #
         ###################
-        for y, tag_arr in enumerate(tags_list):
-            seas_type, xeid, bs4_tag = tag_arr
-            print('\n\n\n for y, tag_arr in enumerate(tags_list): \n\n\n')
-            ###################
-            #   check xeid    #
-            ###################
-            xgxs = get_xhash_score(xeid)
-            if xgxs is True:
-                print('Xeid: %s exist' % xeid)
+        xgxs = get_xhash_score(xeid)
+        if xgxs is True:
+            print('Xeid: %s exist' % xeid)
 
-            #######################
-            #   match from tags   #
-            #######################
-            else:
-                match = rows_to_dict(bs4_tag)
-                print('\n\n\n match = rows_to_dict(bs4_tag) \n\n\n')
-elif args['last'] is True:
-    pass
-
-elif args['each'] is True:
-    # last resalts for all modules
-    pass
-
-else:
-    print('ERROR! Chose statement')
+        #######################
+        #   match from tags   #
+        #######################
+        else:
+            match = rows_to_dict(bs4_tag)
+            print('\n\n\n match = rows_to_dict(bs4_tag) \n\n\n')
